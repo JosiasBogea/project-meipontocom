1edf42bfdc4f80f5582400c98fd0ad928b96724f
JosiasBogea <josiasbn@gmail.com>
2021-09-23 23:58:47 -0300

    Atualização projeto Cesta de Verduras

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 
 import React from 'react';
-import { StatusBar, SafeAreaView, } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Cesta />
+      <StatusBar/>
+      <Cesta/>
     </SafeAreaView>
   );
 }
